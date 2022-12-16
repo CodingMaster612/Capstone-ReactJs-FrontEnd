c8c3da3e131333778206c9f4ff77df787e4fc5b6
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-12-16 14:30:08 -0600

    log

--- capstone/src/components/pages/LoginPage.js
+++ capstone/src/components/pages/LoginPage.js
@@ -5,45 +5,47 @@ import '../../App.css'
 
 export default function SignInPage() {
     return (
-        <div className="text-center m-5-auto">
+        <div className="m-5-auto">
             <div className="wrapper">
-            <div className="change-text">
-            <h2></h2>
+                <div className="change-text">
+                    
+                </div>
+                <form action="/home">
+
+                    <div className="credentials-user">
+                        <p>
+
+                            <div>
+                                <input type="text" name="first_name" className="my-text" placeholder="Enter Your Username or email Address..." required />
+
+                            </div>
+                        </p>
+                    </div>
+                    <div className="credentials-pass">
+                        <p>
+
+                            
+
+                            <Link to="/forget-password"><label className="right-label"></label></Link>
+                            <br />
+                            <input type="password" name="password" className="my-text" placeholder="Enter your password..."required />
+                        </p>
+                    </div>
+
+                    <div className="btn-submit">
+                        <p>
+                            <button type="submit" className="btn-text">Login</button>
+                        </p>
+                    </div>
+                </form>
+                <div className="account-footer">
+                <footer>
+                    <p>First time? <Link to="/register">Create an account</Link>.</p>
+                    <p><Link to="/">Back to Homepage</Link>.</p>
+                </footer>
+                </div>
             </div>
-            <form action="/home">
-                
-                <div className= "credentials-user">
-                <p>
-                    <label></label><br/>
-                    
-                    <input type="text" name="first_name"  className="my-text" required />
-                   
-                </p>
-                </div>
-                <div className="credentials-pass">
-                <p>
-                    
-                    <label></label>
-                    
-                    <Link to="/forget-password"><label className="right-label"></label></Link>
-                    <br/>
-                    <input type="password" name="password"  className="my-text"required />
-                </p>
-                </div>
-                
-                <div className="btn-submit">
-                <p>
-                    <button type="submit" className="btn-text">Login</button>
-                </p>
-                </div>
-            </form>
-            
-            <footer>
-                <p>First time? <Link to="/register">Create an account</Link>.</p>
-                <p><Link to="/">Back to Homepage</Link>.</p>
-            </footer>
-            </div>
-            
+
         </div>
     )
 }
