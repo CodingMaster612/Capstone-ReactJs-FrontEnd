432f3b91241e48e6f02a4f018775c9f5903d3165
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-12-16 12:27:19 -0600

    log

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,54 +1,51 @@
-import React, { useState } from "react";
-import logo from './logo.svg';
-import './App.css';
-import { Login } from "./Login";
-import { Register } from "./Register";
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import LandingPage from './components/pages/LandingPage.js'
+import LoginPage from './components/pages/LoginPage.js'
+import RegisterPage from './components/pages/RegisterPage.js'
+import ForgetPasswordPage from './components/pages/ForgetPasswordPage.js'
+import HomePage from './components/pages/HomePage.js'
 
-function App() {
-  const [currentForm, setCurrentForm] = useState('login');
+import './App.css'
 
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
+export default function App() {
+    return (
+       
+       <BrowserRouter>
+       
+       
+            
+                <Routes>
+                    <Route exact path="/" element={< LandingPage/> } />
+                    <Route path="/login" element={ <LoginPage/> } />
+                    <Route path="/register" element={< RegisterPage/> } />
+                    <Route path="/forget-password" element={ <ForgetPasswordPage/> } />
+                    <Route path="/home" element={ <HomePage/> } />
+                </Routes>
+                
+            
 
-  
 
-  return (
-    
-   
-  
         
 
-        <div className="App">
-      {
-
-        
-      currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-     
-      }
-   <div>
-
-    <Route path="/home" element= {<Login/>} />
-
-
-   </div>
-   
-   
-   
-   
-    </div>
-      
-
-   
-    
-    
-    
-    
-    
-    
-
-    
-  );
+</BrowserRouter>
+    )
 }
 
-export default App;
+const Footer = () => {
+    return (
+        <p className="text-center" style={ FooterStyle }>Designed & coded by <a href="https://izemspot.netlify.com" target="_blank" rel="noopener noreferrer">IZEMSPOT</a></p>
+    )
+}
+
+const FooterStyle = {
+    background: "#222",
+    fontSize: ".8rem",
+    color: "#fff",
+    position: "absolute",
+    bottom: 0,
+    padding: "1rem",
+    margin: 0,
+    width: "100%",
+    opacity: ".5"
+}
