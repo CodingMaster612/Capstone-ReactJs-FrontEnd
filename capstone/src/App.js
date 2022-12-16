1734a1ab0905c187fac32738280fab79138425e8
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-12-16 10:59:48 -0600

    log

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,24 +1,22 @@
-import Register from './Register';
-import Login from './Login';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import React, { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+import { Login } from "./Login";
+import { Register } from "./Register";
+
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
 
   return (
-    <main className="App">
-     <BrowserRouter>
-  {/* <AppContainer> */}
-
-      <Routes>
-       
-        <Route path="/" element={<Login/>} />
-        <Route path="/name" element={ <Register/>} />
-
-      </Routes>
-
-      
-    </BrowserRouter>
-
-    </main>
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
+    </div>
   );
 }
 
