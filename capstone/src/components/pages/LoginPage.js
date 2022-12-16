acc529f83e40f3d5ac1b86f225d0367cfe4aa0ca
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-12-16 14:04:31 -0600

    log

--- capstone/src/components/pages/LoginPage.js
+++ capstone/src/components/pages/LoginPage.js
@@ -8,13 +8,13 @@ export default function SignInPage() {
         <div className="text-center m-5-auto">
             <div className="wrapper">
             <div className="change-text">
-            <h2>Sign in to us</h2>
+            <h2></h2>
             </div>
             <form action="/home">
                 
                 <div className= "credentials-user">
                 <p>
-                    <label>Username or email address</label><br/>
+                    <label></label><br/>
                     
                     <input type="text" name="first_name"  className="my-text" required />
                    
@@ -23,9 +23,9 @@ export default function SignInPage() {
                 <div className="credentials-pass">
                 <p>
                     
-                    <label>Password</label>
+                    <label></label>
                     
-                    <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
+                    <Link to="/forget-password"><label className="right-label"></label></Link>
                     <br/>
                     <input type="password" name="password"  className="my-text"required />
                 </p>
@@ -33,10 +33,11 @@ export default function SignInPage() {
                 
                 <div className="btn-submit">
                 <p>
-                    <button id="sub_btn" type="submit">Login</button>
+                    <button type="submit" className="btn-text">Login</button>
                 </p>
                 </div>
             </form>
+            
             <footer>
                 <p>First time? <Link to="/register">Create an account</Link>.</p>
                 <p><Link to="/">Back to Homepage</Link>.</p>
