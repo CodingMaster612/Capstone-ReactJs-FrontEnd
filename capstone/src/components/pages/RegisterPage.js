bc2d147e103e4cb20782f3f4fdc894169b4c1c1b
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-12-16 23:16:50 -0600

    log

--- capstone/src/components/pages/RegisterPage.js
+++ capstone/src/components/pages/RegisterPage.js
@@ -8,42 +8,42 @@ export default function SignUpPage() {
     return (
         <div className="m-5-auto">
             <div className="wrapper">
-            <h2>Join us</h2>
-            <h5>Create your personal account</h5>
-            <form action="/home">
-                
-                <div className="credentials-user">
-                <p>
-                    <label>Username</label><br/>
-                    <input type="text" name="first_name" required />
-                </p>
-                
-                
-                
-                <p>
-                    <label>Email address</label><br/>
-                    <input type="email" name="email" required />
-                </p>
+                <h2>Join us</h2>
+                <h5>Create your personal account</h5>
+                <form action="/home">
 
-                </div>
-               <div className="credentials-pass">
-                <p>
-                    <label>Password</label><br/>
-                    <input type="password" name="password" required />
-                </p>
+                    <div className="credentials-user">
+                        <p>
+                           
+                            <input type="text" name="first_name"  className="my-text"required />
+                        </p>
 
-                </div>
-                <p>
-                    <input type="checkbox" name="checkbox" id="checkbox" required /> <span>I agree all statements in <a href="https://google.com" target="_blank" rel="noopener noreferrer">terms of service</a></span>.
-                </p>
-                <p>
-                    <button id="sub_btn" type="submit">Register</button>
-                </p>
-            </form>
-            <footer>
-                <p><Link to="/">Back to Homepage</Link>.</p>
-            </footer>
-        </div>
+
+
+                        <p>
+                           
+                            <input type="email" name="email" className="my-text" required />
+                        </p>
+
+                    </div>
+                    <div className="credentials-pass">
+                        <p>
+                            
+                            <input type="password" name="password" className="my-text" required />
+                        </p>
+
+                    </div>
+                    <p>
+                        <input type="checkbox" name="checkbox" id="checkbox" required /> <span>I agree all statements in <a href="https://google.com" target="_blank" rel="noopener noreferrer">terms of service</a></span>.
+                    </p>
+                    <p>
+                        <button id="sub_btn" type="submit" className="my-text">Register</button>
+                    </p>
+                </form>
+                <footer>
+                    <p><Link to="/">Back to Homepage</Link>.</p>
+                </footer>
+            </div>
         </div>
     )
 
