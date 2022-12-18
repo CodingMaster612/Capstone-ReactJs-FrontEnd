a5f7f513312baa32b58e0b5ab82f800b0f0cdc44
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-12-18 14:26:19 -0600

    log

--- capstone/src/components/pages/LoginPage.js
+++ capstone/src/components/pages/LoginPage.js
@@ -8,52 +8,65 @@ import '../../App.css'
 
 export default function SignInPage() {
     return (
-        <header style={ HeaderStyle }>
-       
-       <div className="wrapper">
-            
-        
-            <img src={PillowImage} alt="pillow"></img>
-            
-            <h2>Welcome To Pillow</h2>
-            
-            <form action="/login">
-                <p>
-                   <div className="user-container">
-                    <input type="text" name="first_name" required  placeholder=" Email..."/>
-                    </div>
-                </p>
-                <p>
-                    
-                    <div className="pass-container">
-                    <input type="password" name="password"  placeholder="Password..."required />
-                    </div>
-                    <div>
-                    <Link to="/forget-password"><label className="right-label">Forgot your password?</label></Link>
-                    </div>
-                </p>
-                <p>
-                    <button id="sub_btn" type="submit">Login</button>
-                    
-                    
-                    
-                    
-                    <a>
-                        <FontAwesomeIcon icon={faFacebook}  className="icon-container"/>
-                    </a>
-                    <a>
-                        <FontAwesomeIcon icon={faInstagram}className="icon-container"/> 
-                    </a>
-                    <a>
-                        <FontAwesomeIcon icon={faTwitter} className="icon-container"/> 
-                    </a>
-                </p>
-            </form>
-            <footer>
-                <p>First time? <Link to="/register">Create an account</Link>.</p>
-                <p><Link to="/">Back to Homepage</Link>.</p>
-            </footer>
-        </div>
+        <header style={HeaderStyle}>
+
+            <div className="searchbar-header">
+                <div className="topnav">
+
+                    <a class="active" href="/home">Home</a>
+                    <a href="">About</a>
+                    <a href="">Contact</a>
+                    {/* <input type="search" id="search" placeholder="Search" /> */}
+                </div>
+
+
+
+            </div>
+
+            <div className="wrapper">
+
+
+                <img src={PillowImage} alt="pillow"></img>
+
+                <h2>Welcome To Pillow</h2>
+
+                <form action="/login">
+                    <p>
+                        <div className="user-container">
+                            <input type="text" name="first_name" required placeholder=" Email..." />
+                        </div>
+                    </p>
+                    <p>
+
+                        <div className="pass-container">
+                            <input type="password" name="password" placeholder="Password..." required />
+                        </div>
+                        <div>
+                            <Link to="/forget-password"><label className="right-label">Forgot your password?</label></Link>
+                        </div>
+                    </p>
+                    <p>
+                        <button id="sub_btn" type="submit">Login</button>
+
+
+
+
+                        <a>
+                            <FontAwesomeIcon icon={faFacebook} className="icon-container" />
+                        </a>
+                        <a>
+                            <FontAwesomeIcon icon={faInstagram} className="icon-container" />
+                        </a>
+                        <a>
+                            <FontAwesomeIcon icon={faTwitter} className="icon-container" />
+                        </a>
+                    </p>
+                </form>
+                <footer>
+                    <p>First time? <Link to="/register">Create an account</Link>.</p>
+                    <p><Link to="/">Back to Homepage</Link>.</p>
+                </footer>
+            </div>
         </header>
     )
 }
