057c50caa0357a72a7eef55f877ccc4b5df3c67d
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-12-18 10:36:53 -0600

    log

--- capstone/src/components/pages/LoginPage.js
+++ capstone/src/components/pages/LoginPage.js
@@ -1,34 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import BackgroundImage from '../../assets/images/x.png'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faFacebook,  faInstagram, faTwitter} from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import '../../App.css'
 
 export default function SignInPage() {
     return (
-        <div className="text-center m-5-auto">
+        <header style={ HeaderStyle }>
+       
+       <div className="wrapper">
             <h2>Sign in to us</h2>
-            <form action="/home">
+            <form action="/login">
                 <p>
-                    <label>Username or email address</label><br/>
+                    <label>Username or email address</label><br />
                     <input type="text" name="first_name" required />
-                    <a href="https://www.facebook.com">
-                        <FontAwesomeIcon icon={faFacebook} />
-                        </a>
-                    <FontAwesomeIcon icon={faInstagram} />
-                    <FontAwesomeIcon icon={faTwitter} />
 
-                    
-                    </p>
+                </p>
                 <p>
                     <label>Password</label>
                     <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
-                    <br/>
+                    <br />
                     <input type="password" name="password" required />
                 </p>
                 <p>
                     <button id="sub_btn" type="submit">Login</button>
+                    <a href="https://www.facebook.com">
+                        <FontAwesomeIcon icon={faFacebook}  className="icon-container"/>
+                    </a>
+                    <a href="https://www.instagram.com">
+                        <FontAwesomeIcon icon={faInstagram}className="icon-container"/> 
+                    </a>
+                    <a href="https:/www.twitter.com">
+                        <FontAwesomeIcon icon={faTwitter} className="icon-container"/> 
+                    </a>
                 </p>
             </form>
             <footer>
@@ -36,5 +42,14 @@ export default function SignInPage() {
                 <p><Link to="/">Back to Homepage</Link>.</p>
             </footer>
         </div>
+        </header>
     )
+}
+const HeaderStyle = {
+    width: "100%",
+    height: "100vh",
+    background: `url(${BackgroundImage})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover"
 }
