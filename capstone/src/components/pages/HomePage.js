aff393f341ce16bf0191ea8cc4a9e29908f88b53
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-12-19 10:53:06 -0600

    log

--- capstone/src/components/pages/HomePage.js
+++ capstone/src/components/pages/HomePage.js
@@ -1,48 +1,67 @@
-import { React} from "react";
+import { React } from "react";
 import { Link } from 'react-router-dom'
 
 import TextField from "@mui/material/TextField";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch , faHouse} from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faHouse , faUser, faBars , faWallet} from "@fortawesome/free-solid-svg-icons";
 import '../../App.css'
 import BackgroundImage from '../../assets/images/pink2.png'
 export default function HomePage() {
-return (
-<header style={HeaderStyle}>
-   <div className="searchbar-header">
-      {/* <div className="topnav">
-         <a className="active" href="/home">Place holder</a>
-         <a  href="/home">Place Holder</a> */}
-         <Link to="/home">
-         <div className="icon-home">
-         <FontAwesomeIcon icon={faHouse}/>
+   return (
+      <header style={HeaderStyle}>
+         <div className="searchbar-header">
+            
+            <Link to="/home">
+               <div className="icon-home">
+                  <FontAwesomeIcon icon={faHouse} />
+               </div>
+            </Link>
+            <div>
+               <Link to="/profile">
+                  <div className="icon-profile">
+                     <FontAwesomeIcon icon={faUser} />
+                  </div>
+               </Link>
+            </div>
+
+            <div>
+               <Link to="/list">
+                  <div className="icon-bar">
+                     <FontAwesomeIcon icon={faBars} />
+                  </div>
+               </Link>
+            </div>
+            <div>
+               <Link to="/wallet">
+                  <div className="icon-wallet">
+                     <FontAwesomeIcon icon={faWallet} />
+                  </div>
+               </Link>
+            </div>
+            
          </div>
-         </Link>
-         {/* insert icons into header */}
-      {/* </div> */}
-   </div>
-   <div className="main">
-      <div className="icon">
-         <FontAwesomeIcon icon={faSearch} />
-      </div>
-      <div className="search">
-         {/* add search filter here as an input tag */}
-         <TextField
-            id="outlined-basic"
-            variant="outlined"
-            fullWidth
-            label="Search"
-            />
-      </div>
-   </div>
-</header>
-)
+         <div className="main">
+            <div className="icon">
+               <FontAwesomeIcon icon={faSearch} />
+            </div>
+            <div className="search">
+               {/* add search filter here as an input tag */}
+               <TextField
+                  id="outlined-basic"
+                  variant="outlined"
+                  fullWidth
+                  label="Search"
+               />
+            </div>
+         </div>
+      </header>
+   )
 }
 const HeaderStyle = {
-width: "100%",
-height: "100vh",
-background: `url(${BackgroundImage})`,
-backgroundPosition: "center",
-backgroundRepeat: "no-repeat",
-backgroundSize: "cover"
+   width: "100%",
+   height: "100vh",
+   background: `url(${BackgroundImage})`,
+   backgroundPosition: "center",
+   backgroundRepeat: "no-repeat",
+   backgroundSize: "cover"
 }
