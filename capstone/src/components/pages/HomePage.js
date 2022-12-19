046cfc3dba84ca46aafed0ef1d7f604e3300f1a7
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-12-19 07:36:55 -0600

    log

--- capstone/src/components/pages/HomePage.js
+++ capstone/src/components/pages/HomePage.js
@@ -2,60 +2,40 @@ import { React} from "react";
 import TextField from "@mui/material/TextField";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch} from "@fortawesome/free-solid-svg-icons";
-
-
 import '../../App.css'
-
 import BackgroundImage from '../../assets/images/pink2.png'
-
 export default function HomePage() {
-
-
-    return (
-
-        <header style={HeaderStyle}>
-            <div className="searchbar-header">
-                <div className="topnav">
-
-                    <a class="active" href="/home">Place holder</a>
-                    <a  href="/home">Place Holder</a>
-                    
-
-
-                </div>
-
-
-            </div>
-
-            <div className="main">
-            <div className="icon">
-                
-                <FontAwesomeIcon icon={faSearch} />
-                </div>
-                <div className="search">
-                
-                    
-                    <TextField
-                        id="outlined-basic"
-
-                        variant="outlined"
-                        fullWidth
-                        label="Search"
-                    />
-                </div>
-
-            </div>
-
-        </header>
-
-    )
+return (
+<header style={HeaderStyle}>
+   <div className="searchbar-header">
+      <div className="topnav">
+         <a class="active" href="/home">Place holder</a>
+         <a  href="/home">Place Holder</a>
+         {/* insert icons into header */}
+      </div>
+   </div>
+   <div className="main">
+      <div className="icon">
+         <FontAwesomeIcon icon={faSearch} />
+      </div>
+      <div className="search">
+         {/* add search filter here as an input tag */}
+         <TextField
+            id="outlined-basic"
+            variant="outlined"
+            fullWidth
+            label="Search"
+            />
+      </div>
+   </div>
+</header>
+)
 }
 const HeaderStyle = {
-    width: "100%",
-    height: "100vh",
-    background: `url(${BackgroundImage})`,
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover"
+width: "100%",
+height: "100vh",
+background: `url(${BackgroundImage})`,
+backgroundPosition: "center",
+backgroundRepeat: "no-repeat",
+backgroundSize: "cover"
 }
-
