bad7b059369ad4aaa21df6d7947c3a8ce7751139
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-12-19 10:35:32 -0600

    log

--- capstone/src/components/pages/HomePage.js
+++ capstone/src/components/pages/HomePage.js
@@ -1,18 +1,25 @@
 import { React} from "react";
+import { Link } from 'react-router-dom'
+
 import TextField from "@mui/material/TextField";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch} from "@fortawesome/free-solid-svg-icons";
+import { faSearch , faHouse} from "@fortawesome/free-solid-svg-icons";
 import '../../App.css'
 import BackgroundImage from '../../assets/images/pink2.png'
 export default function HomePage() {
 return (
 <header style={HeaderStyle}>
    <div className="searchbar-header">
-      <div className="topnav">
-         <a class="active" href="/home">Place holder</a>
-         <a  href="/home">Place Holder</a>
+      {/* <div className="topnav">
+         <a className="active" href="/home">Place holder</a>
+         <a  href="/home">Place Holder</a> */}
+         <Link to="/home">
+         <div className="icon-home">
+         <FontAwesomeIcon icon={faHouse}/>
+         </div>
+         </Link>
          {/* insert icons into header */}
-      </div>
+      {/* </div> */}
    </div>
    <div className="main">
       <div className="icon">
